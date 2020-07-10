de89269d864f754e2ddc4e18c75c40e4d291da33
James Cole <thegrumpydictator@gmail.com>
2020-07-10 06:44:47 +0200

    Update meta files for new release.

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * TrimStrings.php
  * Copyright (c) 2020 james@firefly-iii.org
@@ -19,6 +20,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
