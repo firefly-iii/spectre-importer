acb59c49f2869f66fd72ced2cabe89279cde5a0f
James Cole <james@firefly-iii.org>
2020-11-29 12:02:24 +0100

    Update meta files.

--- config/spectre.php
+++ config/spectre.php
@@ -28,7 +28,7 @@ if ('/' === substr($fireflyIIIURL, -1)) {
 }
 
 return [
-    'version'             => '2.0.0',
+    'version'             => '2.1.0',
     'access_token'        => env('FIREFLY_III_ACCESS_TOKEN'),
     'url'                 => $fireflyIIIURL,
     'vanity_url'          => envNonEmpty('VANITY_URL'),
